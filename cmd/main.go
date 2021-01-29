7d59b6ed318cee503d18d04c96d014c5a07c035d
Isaac Rogers <therealplato@gmail.com>
2021-01-29 15:26:04 -0500

    repotime: add commits and events to timeline

--- cmd/main.go
+++ cmd/main.go
@@ -17,31 +17,19 @@ func main() {
 	token := githubauth.MustAuthorize(os.Stdout)
 	tc := oauth2.NewClient(ctx, oauth2.StaticTokenSource(token))
 	client := github.NewClient(tc)
-	_ = client
-
-	/*
-		repos, _, err := client.Repositories.List(ctx, "", nil)
-		if err != nil {
-			fmt.Print("failed to perform github repository list: %q\n", err)
-			os.Exit(1)
-		}
-		bb, err := json.MarshalIndent(repos, "", "  ")
-		if err != nil {
-			fmt.Print("failed to marshal github repository list: %q\n", err)
-			os.Exit(1)
-		}
-		fmt.Print(string(bb))
-	*/
 	apiServer := &apiServer{client: client}
 	mux := http.NewServeMux()
 	mux.Handle("/api/", apiServer)
 	mux.Handle("/", http.FileServer(http.Dir("./public_http")))
+	fmt.Println("It's repotime! Go to http://localhost:2992")
 	http.ListenAndServe("0.0.0.0:2992", mux)
 }
 
 type apiServer struct {
 	client           *github.Client
 	mux              *http.ServeMux
+	ownerLogin       string
+	repoName         string
 	chosenRepository *github.Repository
 }
 
@@ -50,13 +38,14 @@ func (s *apiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.mux = http.NewServeMux()
 		s.mux.HandleFunc("/api/username", s.getUsername)
 		s.mux.HandleFunc("/api/repositories", s.getRepos)
+		s.mux.HandleFunc("/api/commits", s.getCommits)
+		s.mux.HandleFunc("/api/issues", s.getIssues)
 		s.mux.HandleFunc("/api/set-repository", s.setRepo)
 	}
 	s.mux.ServeHTTP(w, r)
 }
 
 func (s *apiServer) getUsername(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("retrieving username")
 	u, _, err := s.client.Users.Get(r.Context(), "")
 	if err != nil {
 		fmt.Printf("getUsername error: %q\n", err)
@@ -73,7 +62,6 @@ func (s *apiServer) getUsername(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *apiServer) getRepos(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("retrieving repos")
 	rr, _, err := s.client.Repositories.List(r.Context(), "", nil)
 	if err != nil {
 		fmt.Printf("getRepos error: %q\n", err)
@@ -90,7 +78,6 @@ func (s *apiServer) getRepos(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *apiServer) setRepo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("setting repo")
 	repo := &github.Repository{}
 	err := json.NewDecoder(r.Body).Decode(repo)
 	if err != nil {
@@ -99,5 +86,49 @@ func (s *apiServer) setRepo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s.chosenRepository = repo
+	s.ownerLogin = *repo.GetOwner().Login
+	s.repoName = *repo.Name
 	fmt.Printf("repo has been set to %v\n", *repo.FullName)
+}
+
+func (s *apiServer) getCommits(w http.ResponseWriter, r *http.Request) {
+	if s.chosenRepository == nil {
+		fmt.Println("cannot get commits until repository is chosen")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	cc, _, err := s.client.Repositories.ListCommits(r.Context(), s.ownerLogin, s.repoName, nil)
+	if err != nil {
+		fmt.Printf("failed to retrieve commits: %q\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	bb, err := json.MarshalIndent(cc, "", "  ")
+	if err != nil {
+		fmt.Printf("getCommits marshal error: %q\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(bb)
+}
+
+func (s *apiServer) getIssues(w http.ResponseWriter, r *http.Request) {
+	if s.chosenRepository == nil {
+		fmt.Println("cannot get issues until repository is chosen")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	ii, _, err := s.client.Activity.ListIssueEventsForRepository(r.Context(), s.ownerLogin, s.repoName, nil)
+	if err != nil {
+		fmt.Printf("failed to retrieve issues: %q\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	bb, err := json.MarshalIndent(ii, "", "  ")
+	if err != nil {
+		fmt.Printf("getIssues marshal error: %q\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(bb)
 }
--- public_http/app.js
+++ public_http/app.js
@@ -1,12 +1,61 @@
 const headers = { "Content-Type": "application/json" };
+const combineCommitsAndIssues = (commits, issues) => {
+  let combined = [];
+  for (commit of commits) {
+    combined.push({
+      time: commit.commit.committer.date,
+      type: "commit",
+      message: commit.commit.message,
+      url: commit.html_url,
+    });
+  }
+  for (issue of issues) {
+    let item = {};
+    if (issue.issue.pull_request && issue.issue.pull_request.url) {
+      if (issue.event == "closed") {
+        item.type = "pr_closed";
+        item.url = issue.issue.pull_request.html_url;
+      } else if (issue.event == "merged") {
+        item.type = "pr_merged";
+        item.url = issue.issue.pull_request.html_url;
+      } else {
+        continue;
+      }
+    } else {
+      if (issue.event == "closed") {
+        item.type = "issue_closed";
+        item.url = issue.issue.html_url;
+      } else {
+        continue;
+      }
+    }
+    item.message = issue.issue.title;
+    item.time = issue.created_at;
+    combined.push(item);
+  }
+  combined.sort((a, b) => {
+    return new Date(a.time) - new Date(b.time);
+  });
+  return combined;
+};
 const Timeline = {
   data() {
     return {
       username: "",
       reposAvailable: false,
+      commitsAvailable: false,
+      issuesAvailable: false,
+      timelineDataAvailable: false,
       selectedRepo: null,
       repos: [],
+      commits: [],
+      issues: [],
     };
+  },
+  computed: {
+    timelineData() {
+      return combineCommitsAndIssues(this.commits, this.issues);
+    },
   },
 
   created() {
@@ -24,8 +73,8 @@ const Timeline = {
 
   methods: {
     onRepoSelected(event) {
+      this.timelineDataAvailable = false;
       repoName = event.target.value;
-      console.log(repoName);
       let selectedRepo = {};
       for (repo of this.repos) {
         if (repo.full_name === repoName) {
@@ -43,6 +92,21 @@ const Timeline = {
         .catch((e) => {
           console.error(e);
         });
+
+      fetch("/api/commits", { headers })
+        .then((response) => response.json())
+        .then((data) => {
+          this.commitsAvailable = true;
+          this.commits = data;
+        });
+
+      fetch("/api/issues", { headers })
+        .then((response) => response.json())
+        .then((data) => {
+          this.issuesAvailable = true;
+          this.issues = data;
+        });
+      this.timelineDataAvailable = true;
     },
   },
 };
